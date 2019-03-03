74c1219e6c62012d3c8aec35285a9d30e396be6d
Guillaume Van Wassenhove <Guillaume.VanWassenhove@UGent.be>
2019-03-03 16:36:57 +0100

    Implement a very basic MrX AI which tries to maximize the distance to the detectives. Black cards and double moves aren't used (yet).

--- ai/human/misterx.py
+++ ai/human/misterx.py
@@ -1,9 +1,31 @@
 from game.misterx import MisterX
+import random
+import networkx as nx
 
-class ExampleAIImplementationMisterX(Misterx):
+
+class ExampleAIImplementationMisterX(MisterX):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    
+
     # Should return a tuple (destination:int, transportation:string)
     def decide(self):
-        return 153, 'taxi'
+
+        # get options for MrX
+        mrxOptions = self.game.board.getOptions(self, doubleAllowed=False)
+        print(f"Mr. X his options are:: {mrxOptions}")
+
+        # give scores to each neighbour based on the shortest path of each neighbour to the detectives (higher score is better)
+        scores = [(nbr, min(nx.shortest_path_length(self.game.board.graph, nbr, detective.position) for detective in self.game.detectives))
+                  for nbr in self.game.board.graph[self.position]]
+        print(f"Scores for each neighbour of MrX {scores}")
+        scores = [tuple for tuple in scores if tuple[1] == max(score[1] for score in scores)]
+        print(f"Maximal scores for neighbours of MrX {scores}")
+
+        # determine options with maximal scores
+        bestOptions = [option for option in mrxOptions if option[0] in [score[0] for score in scores]]
+        print(f"Choices with maximal scores: {bestOptions}")
+
+        # choose random option from options with maximal scores
+        decision = random.choice(bestOptions)
+        print(f"{self} chose to move from {self.position} to {decision[0]} via {decision[1]}")
+        return decision[0], decision[1]
