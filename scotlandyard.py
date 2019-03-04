d3660faf9880964c049fa204b6a0fed0a0fe6a8b
Guillaume Van Wassenhove <Guillaume.VanWassenhove@UGent.be>
2019-03-04 22:09:49 +0100

    MrX AI; Add black card usage after location reveal

--- scotlandyard.py
+++ scotlandyard.py
@@ -5,19 +5,21 @@ import display.gui as gui
 
 import game.constants as const
 import game.util as util
+from ai.human import misterx
 
 
 def main():
     app = gui.createApp(sys.argv)
 
     game = ScotlandYard(visualize=True, verbose=True)
+    game.addMisterX(misterx.ExampleAIImplementationMisterX(game=game, name="AI Mister X", blackCards=4))
     guiInstance = gui.createGui(game)
 
     stop = False
     while not stop:
         stop, status = game.update()
         pass  # Visualization function calls could be added here
-    
+
     print(f"Game ended with status {status}::  {const.GAME_END_MESSAGES[status]}")
 
     # Please linter, use app and guiInstance somewhere
