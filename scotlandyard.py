1d6568094c6d2096f00b3944be3f1df9adb83fd5
Jonas Vander Vennet <jonas.vdv@hotmail.com>
2019-03-07 15:18:17 +0100

    Update start script

--- scotlandyard.py
+++ scotlandyard.py
@@ -1,36 +1,31 @@
-import sys
-import configparser
-
 from game.game import ScotlandYard
-from ai.human import misterx
+from ai.human import misterx, detective, random
 import display.gui as gui
 
 import game.util as util
 
 
-from ai.human.detective import ExampleAIImplementationDetective
 def main():
-    app = gui.createApp(sys.argv)
-    config = configparser.ConfigParser()
-    config.read('settings.ini')
-
-    if len(config.keys()) == 1:  # File doesn't exist, only default key present
-        config = util.generateDefaultConfig(config)
-
+    config = util.readConfig('settings.ini')
+    
     game = ScotlandYard(cfg=config)
     game.addMisterX(misterx.ExampleAIImplementationMisterX(game=game, name="AI Mister X", blackCards=4))
-    game = ScotlandYard(visualize=True, verbose=True)
-    game.addDetectives([ExampleAIImplementationDetective(name=f"Detective{i+1}", game=ScotlandYard()) for i in range(4)])
+    # game.addDetectives([detective.ExampleAIImplementationDetective(name=f"Detective{i+1}", game=game) for i in range(4)])
+    # game.addDetectives([random.ExampleAIImplementationRandomDetective(name=f"Detective{i+1}", game=game) for i in range(4)])
 
-    guiInstance = gui.createGui(game)
+    if config['OUTPUT'].getboolean('visualization'):
+        app = gui.createApp([])
+        guiInstance = gui.createGui(game)
 
-    if config['DISPLAY'].getboolean('multithreaded_drawing'):
-        app.exec()
+        if config['DISPLAY'].getboolean('multithreaded_drawing'):
+            app.exec()
+        else:
+            game.loop()
+
+        # Please linter, use app and guiInstance somewhere
+        util.clear([app, guiInstance])
     else:
         game.loop()
-
-    # Please linter, use app and guiInstance somewhere
-    util.clear([app, guiInstance])
 
 
 if __name__ == '__main__':
