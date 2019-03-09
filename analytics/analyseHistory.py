5d93e40b3e37d2609f7804b46cf3729a47a9a11e
Jonas Vander Vennet <jonas.vdv@hotmail.com>
2019-03-09 12:32:58 +0100

    analytics; Add support for zip file reading

--- analytics/analyseHistory.py
+++ analytics/analyseHistory.py
@@ -1,24 +1,42 @@
 import os
 import numpy as np
 
-basepath = os.getcwd()
-basepath = basepath.split('\\analytics')[0]
-basepath += "\\history"
 
-r = [0, 0]
-ignorelist = ["zip"]
+def score(arr):
+    if sum(arr) != 0:
+        return f"\tDetectives: {arr[0]}; ({arr[0]/sum(arr)*100}%)\n\tMrx: {arr[1]}; ({arr[1]/sum(arr)*100}%)\n"
 
-for f in os.listdir(basepath):
-    ignore = False
-    for i in ignorelist:
-        if i in f:
-            ignore = True
-    if ignore:
-        continue
-    fpath = basepath + "\\" + f
-    data = np.load(fpath)
 
+def readFile(data, arr):
     result = data[0]
-    r[result] += 1
+    arr[result] += 1
+    return arr
 
-print(f"Detectives: {r[0]}; ({r[0]/sum(r)*100}%)\nMrx: {r[1]}; ({r[1]/sum(r)*100}%)")
+
+def readZip(filename, data):
+    print()
+    s = [0, 0]
+    for k, v in data.items():
+        s = readFile(v, s)
+    print(f"{filename}:\n{score(s)}")
+
+
+def main():
+    basepath = os.getcwd()
+    basepath = basepath.split('\\analytics')[0]
+    basepath += "\\history"
+
+    r = [0, 0]
+
+    for f in os.listdir(basepath):
+        fpath = basepath + "\\" + f
+        data = np.load(fpath)
+        if "zip" in f:
+            readZip(f, data)
+        else:
+            r = readFile(data, r)
+    print(f"Main folder:\n{score(r)}")
+
+
+if __name__ == '__main__':
+    main()
